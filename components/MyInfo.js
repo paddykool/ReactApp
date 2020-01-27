98c4ba87744633a047133fc1e380fec393c03e26
Paddy <pjmullan@gmail.com>
2020-01-27 23:05:07 +0000

    components in folder

---
+++ components/MyInfo.js
@@ -0,0 +1,18 @@
+
+import React from "react"
+
+function MyInfo () {
+    return (
+        <div> 
+            <h1>Paddy Mullan</h1>
+            <p>Ah... ya all know</p>
+            <ol>
+                <li>Morocco</li>
+                <li>Nepal</li>
+                <li>Africa</li>
+            </ol>
+        </div>
+    );
+}
+
+export default MyInfo
--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function MyFirstComponent () {
-    return (
-        <div> 
-            <h1>Paddy Mullan</h1>
-            <p>Ah... ya all know</p>
-            <ol>
-                <li>Morocco</li>
-                <li>Nepal</li>
-                <li>Africa</li>
-            </ol>
-        </div>
-    );
-}
+import MyInfo from './components/MyInfo';
 
 // JSX
 ReactDOM.render(
-    <MyFirstComponent />, 
+    <MyInfo />, 
     document.getElementById('root')
 );
